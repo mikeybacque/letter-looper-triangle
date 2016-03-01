834e5543bbab4c24e643e0ae8ed8b055abe9e81c
mikeybacque <mikey.bacque@gmail.com>
2016-03-01 12:47:39 -0500

    js loop

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var letter = prompt('Enter a letter:');
+
+var i;
+
+for (i = 0; i < 1; i++) {
+  document.write(letter + '<br>');
+};
+
+for (i = 0; i < 1; i++) {
+  document.write(letter + letter + '<br>');
+};
+
+for (i = 0; i < 1; i++) {
+  document.write(letter + letter + letter + '<br>');
+};
+
+for (i = 0; i < 1; i++) {
+  document.write(letter + letter + letter + letter + '<br>');
+};
+
+for (i = 0; i < 1; i++) {
+  document.write(letter + letter + letter + letter + letter + '<br>');
+};
+
+for (i = 0; i < 1; i++) {
+  document.write(letter + letter + letter + letter + letter + letter + '<br>');
+};
+
+for (i = 0; i < 1; i++) {
+  document.write(letter + letter + letter + letter + letter + letter + letter + '<br>');
+};
+
+for (i = 0; i < 1; i++) {
+  document.write(letter + letter + letter + letter + letter + letter + letter + letter + '<br>');
+};
+
+for (i = 0; i < 1; i++) {
+  document.write(letter + letter + letter + letter + letter + letter + letter + letter + letter + '<br>');
+};
+
+for (i = 0; i < 1; i++) {
+  document.write(letter + letter + letter + letter + letter + letter + letter + letter + letter + letter + '<br>');
+};
